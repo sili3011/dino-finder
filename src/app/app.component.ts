de238858ddc65bdc5db76db8c57b1e34c069a403
Silvio Schmidt <silvio.schmidt@HAGLEITNER.com>
2022-09-09 16:20:26 +0200

    showing 100 excavation sites at a time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import dinos from 'dinos.json';
+import { BoundsLiteral } from '@ng-maps/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dino-finder';
+  initilized = false;
+  lat = 0;
+  long = 0;
+
+  shownLocations: any[] = [];
+
+  private ALL_RECORDS_URL =
+    'https://paleobiodb.org/data1.2/occs/list.json?all_records';
+
+  private db = dinos.records;
+
+  constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude;
+      this.lat = position.coords.longitude;
+      this.initilized = true;
+    });
+    // run node backend once a day to receive new data and distribute it to clients
+    // this.http
+    //   .get('https://paleobiodb.org/data1.2/occs/list.json?all_records')
+    //   .subscribe((rec) => console.log(rec));
+  }
+
+  showMarkers($event: BoundsLiteral): void {
+    this.shownLocations = this.db
+      .filter(
+        (dino) =>
+          dino.lat > $event.south &&
+          dino.lat < $event.north &&
+          dino.lng > $event.west &&
+          dino.lng < $event.east
+      )
+      .slice(0, 100);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgMapsCoreModule } from '@ng-maps/core';
+import { GOOGLE_MAPS_API_CONFIG, NgMapsGoogleModule } from '@ng-maps/google';
+import secrets from 'secrets.json';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMapsCoreModule,
+    NgMapsGoogleModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: GOOGLE_MAPS_API_CONFIG,
+      useValue: {
+        apiKey: secrets.GMAP_API_KEY,
+      },
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

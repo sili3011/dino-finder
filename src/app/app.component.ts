42bd5768a5400b46c69ea553b4a71e5ece6f1e9d
Silvio Schmidt <silvio.schmidt@HAGLEITNER.com>
2022-09-12 16:31:21 +0200

    using googlemaps component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Loader } from '@googlemaps/js-api-loader';
 
 import dinos from 'dinos.json';
-import { BoundsLiteral } from '@ng-maps/core';
+import secrets from 'secrets.json';
+import { GoogleMap, MapInfoWindow } from '@angular/google-maps';
+import { MarkerManager } from '@googlemaps/markermanager';
 
 @Component({
   selector: 'app-root',
@@ -11,40 +14,75 @@ import { BoundsLiteral } from '@ng-maps/core';
 })
 export class AppComponent implements OnInit {
   title = 'dino-finder';
-  initilized = false;
+  loadedScript = false;
   lat = 0;
   long = 0;
 
   shownLocations: any[] = [];
 
+  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+
+  @ViewChild(MapInfoWindow, { static: false })
+  info!: MapInfoWindow;
+
   private ALL_RECORDS_URL =
     'https://paleobiodb.org/data1.2/occs/list.json?all_records';
+  // https://paleobiodb.org/data1.2/occs/list.json?datainfo&rowcount&base_name=Dinosauria&show=coords
 
   private db = dinos.records;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {}
 
   public ngOnInit(): void {
     navigator.geolocation.getCurrentPosition((position) => {
       this.lat = position.coords.latitude;
-      this.lat = position.coords.longitude;
-      this.initilized = true;
+      this.long = position.coords.longitude;
     });
+
+    const loader = new Loader({
+      apiKey: secrets.GMAP_API_KEY,
+      version: 'weekly',
+    });
+
+    loader.load().then(() => {
+      this.loadedScript = true;
+      this.cd.detectChanges();
+
+      const markerManager = new MarkerManager(this.map.googleMap!, {
+        borderPadding: 0,
+        maxZoom: 10,
+        shown: true,
+        trackMarkers: true,
+      });
+
+      //markerManager.addMarker(new google.maps.Marker())
+
+      markerManager.addMarkers(
+        this.db.map(
+          (dino) =>
+            new google.maps.Marker({
+              position: new google.maps.LatLng(dino.lat, dino.lng),
+            })
+        ),
+        1,
+        25
+      );
+    });
+
     // run node backend once a day to receive new data and distribute it to clients
     // this.http
     //   .get('https://paleobiodb.org/data1.2/occs/list.json?all_records')
     //   .subscribe((rec) => console.log(rec));
   }
 
-  showMarkers($event: BoundsLiteral): void {
-    this.shownLocations = this.db
-      .filter(
-        (dino) =>
-          dino.lat > $event.south &&
-          dino.lat < $event.north &&
-          dino.lng > $event.west &&
-          dino.lng < $event.east
-      )
-      .slice(0, 100);
+  public infoContent = '';
+
+  public openInfo(element: any, content: string): void {
+    this.infoContent = content;
+    this.info.open(element);
+  }
+
+  consolo() {
+    console.log(this.map.googleMap!.get('zoom'));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgMapsCoreModule } from '@ng-maps/core';
-import { GOOGLE_MAPS_API_CONFIG, NgMapsGoogleModule } from '@ng-maps/google';
-import secrets from 'secrets.json';
 import { HttpClientModule } from '@angular/common/http';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgMapsCoreModule,
-    NgMapsGoogleModule,
+    GoogleMapsModule,
     HttpClientModule,
   ],
-  providers: [
-    {
-      provide: GOOGLE_MAPS_API_CONFIG,
-      useValue: {
-        apiKey: secrets.GMAP_API_KEY,
-      },
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
